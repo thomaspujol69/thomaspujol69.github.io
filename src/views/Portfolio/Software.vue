3288cfc9ae2299bb6f3326a4dfbbf70479b26033
Thomas PUJOL <thomas.pujol@etu.univ-lyon1.fr>
2021-12-23 01:09:12 +0100

    404 ToDo : Software images

--- src/views/Portfolio/Software.vue
+++ src/views/Portfolio/Software.vue
@@ -1,37 +1,43 @@
 <template>
   <v-container grid-list-xl>
     <h2 class="pl-4">
-      <span>Video</span>
-      <span class="green--text">Portfolio</span>
+      <span class="blue--text">Portfolio</span>
+      <span> Logiciel</span>
     </h2>
     <v-layout row justify-center align-center wrap class="mt-4 pt-2">
       <v-dialog
-        v-model="video.dialog"
+        v-model="project.dialog"
         lazy
-        max-width="800"
-        v-for="video in videos"
-        :key="video.title"
+        max-width="1000"
+        v-for="project in projects"
+        :key="project.title"
       >
         <template v-slot:activator="{ on }">
           <v-flex xs12 sm6 md4 lg4 xl4 v-on="on">
             <v-card hover flat color="transparent">
               <v-img
-                :src="video.poster"
-                aspect-ratio="1.7778"
-                :alt="video.title"
+                :src="project.poster"
+                :alt="project.title"
+                height="230"
                 lazy-src="https://cdn.dribbble.com/users/503653/screenshots/3143656/fluid-loader.gif"
-                contain
               ></v-img>
-              <v-card-title primary-title class="justify-center">{{video.title}}</v-card-title>
+              <v-card-title primary-title class="justify-center">{{project.title}}</v-card-title>
             </v-card>
           </v-flex>
         </template>
-        <v-card v-if="video.dialog">
-          <plyr>
-            <div class="plyr__video-embed">
-              <iframe :src="video.href" allowfullscreen allowtransparency allow="autoplay"></iframe>
-            </div>
-          </plyr>
+        <v-card v-if="project.dialog">
+          <v-img :src="project.poster"></v-img>
+          <v-card-text>
+            <h3 class="headline mb-0">
+              <span>Technologies</span>
+            </h3>
+            <v-chip v-for="t in project.tech" :key="t" color="blue" text-color="white">{{t}}</v-chip>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn v-for="b in project.buttons" :key="b.name" flat large dark color="blue" :href="b.link" target="_blank">
+              <v-icon left>{{ b.icon }}</v-icon>{{ b.name }}
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-dialog>
     </v-layout>
@@ -39,18 +45,16 @@
 </template>
 
 <script>
-import { PlyrVideo } from 'vue-plyr'
-
 export default {
   metaInfo: {
-    title: 'Portfolio Administration système',
+    title: 'Portfolio Logiciel',
     titleTemplate: '%s ← Thomas PUJOL',
     meta: [
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         name: 'description',
         content:
-          'Réalisations de Thomas PUJOL'
+          'Portfolio des logiciels de Thomas PUJOL'
       },
       { charset: 'utf-8' },
       { property: 'og:title', content: 'Thomas PUJOL' },
@@ -64,110 +68,56 @@ export default {
       {
         property: 'og:description',
         content:
-          'Réalisations de Thomas PUJOL'
+          'Portfolio des logiciels de Thomas PUJOL'
       }
     ]
   },
-  components: { plyr: PlyrVideo },
   data () {
     return {
       dialog: false,
-      videos: [
+      projects: [
         {
           dialog: false,
-          title: 'Vila Design Promo video',
-          href: 'https://www.youtube.com/embed/XP8j7NA_wzc',
-          type: 'text/html',
-          youtube: 'XP8j7NA_wzc',
-          poster: 'https://img.youtube.com/vi/XP8j7NA_wzc/maxresdefault.jpg'
+          title: 'Mise en forme de rapports Nessus',
+          tech: ['Python', 'JSON', 'OpenOffice'],
+          poster: '/resources/images/software/nessus.png'
         },
         {
           dialog: false,
-          title: 'Pushartist Promo video',
-          href: 'https://www.youtube.com/embed/PZnn1fDUb6U',
-          type: 'text/html',
-          youtube: 'PZnn1fDUb6U',
-          poster: 'https://img.youtube.com/vi/PZnn1fDUb6U/maxresdefault.jpg'
+          title: 'Cahier de présence',
+          tech: ['Python', 'JSON', 'OpenOffice'],
+          poster: '/resources/images/software/cdp.png'
         },
         {
           dialog: false,
-          title: 'Unbroken Designs Instagram Promo video',
-          href: 'https://www.youtube.com/embed/KfxHI3vwALk',
-          type: 'text/html',
-          youtube: 'KfxHI3vwALk',
-          poster: 'https://img.youtube.com/vi/KfxHI3vwALk/maxresdefault.jpg'
+          title: 'Snake en C & Assembleur',
+          tech: ['Python', 'JSON', 'OpenOffice'],
+          poster: '/resources/images/software/snake.png'
         },
         {
           dialog: false,
-          title: 'EEVOY APP Promo',
-          href: 'https://www.youtube.com/embed/JEhpsP75STk',
-          type: 'text/html',
-          youtube: 'JEhpsP75STk',
-          poster: 'https://i.imgur.com/7CLqR2o.png'
+          title: 'Vérification de disponibilité des chronodoses',
+          tech: ['Python', 'Kivy', 'Doctolib API'],
+          poster: '/resources/images/software/.png'
         },
         {
           dialog: false,
-          title: "Patty Boland's Party video",
-          href: 'https://www.youtube.com/embed/5zB4FIkhFHk',
-          type: 'text/html',
-          youtube: '5zB4FIkhFHk',
-          poster: 'https://img.youtube.com/vi/5zB4FIkhFHk/maxresdefault.jpg'
+          title: 'Doscord bot - synchronisation de rôles',
+          buttons: [
+            {
+              link: 'https://bdeinfo.org/',
+              icon: 'fas fa-desktop',
+              name: 'Demo'
+            }
+          ],
+          tech: ['Python', 'JSON', 'OpenOffice'],
+          poster: '/resources/images/software/.png'
         },
         {
           dialog: false,
-          title: 'Montenegro Summer 2017',
-          href: 'https://www.youtube.com/embed/kZahccWTpGc',
-          type: 'text/html',
-          youtube: 'kZahccWTpGc',
-          poster: 'https://i.imgur.com/4yTFAlM.jpg'
-        },
-        {
-          dialog: false,
-          title: 'VilaShirts Promo video',
-          href: 'https://www.youtube.com/embed/SZVMKtEORRw',
-          type: 'text/html',
-          youtube: 'SZVMKtEORRw',
-          poster: 'https://i.imgur.com/POCbX1B.jpg'
-        },
-        {
-          dialog: false,
-          title: 'Albania Durres Summer 2018',
-          href: 'https://www.youtube.com/embed/jd9pDFaRDKI',
-          type: 'text/html',
-          youtube: 'jd9pDFaRDKI',
-          poster: 'https://img.youtube.com/vi/jd9pDFaRDKI/maxresdefault.jpg'
-        },
-        {
-          dialog: false,
-          title: 'Matricar Instagram Promo video',
-          href: 'https://www.youtube.com/embed/S-2hNuuwmpY',
-          type: 'text/html',
-          youtube: 'S-2hNuuwmpY',
-          poster: 'https://img.youtube.com/vi/S-2hNuuwmpY/maxresdefault.jpg'
-        },
-        {
-          dialog: false,
-          title: 'Vila Design Glitch Logo Animation',
-          href: 'https://www.youtube.com/embed/SrX8T2G6bew',
-          type: 'text/html',
-          youtube: 'SrX8T2G6bew',
-          poster: 'https://img.youtube.com/vi/SrX8T2G6bew/maxresdefault.jpg'
-        },
-        {
-          dialog: false,
-          title: 'Splash logo animation',
-          href: 'https://www.youtube.com/embed/vBAZpwtq08I',
-          type: 'text/html',
-          youtube: 'vBAZpwtq08I',
-          poster: 'https://i.imgur.com/3jK0rTs.png'
-        },
-        {
-          dialog: false,
-          title: 'Julie Lindh CBD Infusion Facial',
-          href: 'https://www.youtube.com/embed/9qFQMV9IJ9I',
-          type: 'text/html',
-          youtube: '9qFQMV9IJ9I',
-          poster: 'https://img.youtube.com/vi/9qFQMV9IJ9I/maxresdefault.jpg'
+          title: 'Autres',
+          tech: ['Python'],
+          poster: '/resources/images/software/others.png'
         }
       ]
     }
@@ -176,5 +126,4 @@ export default {
 </script>
 
 <style  scoped>
-@import "../../../node_modules/vue-plyr/dist/vue-plyr.css";
 </style>
